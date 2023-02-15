c2161cbc3e6fb832d13408d0d74b46d46c09589b
yemi <hawogbesanope1@aol.com>
2023-02-15 14:25:27 -0500

    third commit

--- file1.java
+++ file1.java
@@ -2,3 +2,8 @@ this is a java script
 did yo see this
 this is just crazy
 Macbook is overrated
+Click here to download the latest (2.39.2) 64-bit version of Git for Windows. This is the most recent maintained build. It was released 1 day ago, on 2023-02-14.
+
+Other Git for Windows downloads
+Standalone Installer
+32-bit Git for Windows Setup.
