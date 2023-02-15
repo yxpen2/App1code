294bab0b8b2b56f1737efc8eb56a6d8148ac27d4
yemi <hawogbesanope1@aol.com>
2023-02-15 15:45:36 -0500

    sixth commit

---
+++ file2.java
@@ -0,0 +1,9 @@
+i-061687e933065ad48 (git-sever1)
+Open an SSH client.
+
+Locate your private key file. The key used to launch this instance is gitwindows.pem
+
+Run this command, if necessary, to ensure your key is not publicly viewable.
+ chmod 400 gitwindows.pem
+
+Connect to your instance using its Public DNS:
